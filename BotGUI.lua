db6696f3ca1413743c7dc63c7b3e372ca02e156b
bot21333 <128122327+bot21333@users.noreply.github.com>
2023-12-13 15:32:51 +0400

    Update BotGUI.lua

--- BotGUI.lua
+++ BotGUI.lua
@@ -4617,32 +4617,4 @@ do
 		end
 	end
 end
-
-local UIStroke = Instance.new("UIStroke")
-local UICorner = Instance.new("UICorner")
-
-local ScreenGui = Instance.new("ScreenGui")
-local ImageButton = Instance.new("ImageButton")
-
-local ScreenGui = Instance.new("ScreenGui")
-ScreenGui.Name = "ZekromMinimze"
-ScreenGui.Parent = game.CoreGui
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-
-ImageButton.Parent = ScreenGui
-ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
-ImageButton.Draggable = true
-ImageButton.Position = UDim2.new(0.102097899, 0, 0.0742971897, 0)
-ImageButton.Size = UDim2.new(0, 59, 0, 49)
-ImageButton.Image = "http://www.roblox.com/asset/?id=15617446791"
-
-UICorner.Name = "MainCorner"
-UICorner.CornerRadius = UDim.new(0, 9)
-UICorner.Parent = ImageButton
-ImageButton.MouseButton1Down:connect(function()
-	window:toggle()
-end)
-
 return library
